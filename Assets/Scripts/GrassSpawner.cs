2e070b3b8cab9c72694435e2c6d05452847c38c0
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2021-11-12 23:08:37 +0100

    Grass insect spawner

---
+++ Assets/Scripts/GrassSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassSpawner : MonoBehaviour
+{
+    public GameObject prefab;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player has touched the grass");
+            SpawnInsect();
+        }
+    }
+
+    void SpawnInsect()
+    {
+        Vector3 position = gameObject.transform.position;
+        position.y += 1;
+
+        int isSpawned = Random.Range(1, 30);
+        if (isSpawned <= 1)
+            Instantiate(prefab, position, Quaternion.identity);
+    }
+}
