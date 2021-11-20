8b99da3ee4a3fbf90f4bdec37121dc062a213347
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2021-11-20 17:15:28 +0100

    Replace grass with flowers on collision with seed

---
+++ Assets/Scripts/ReplaceGrass.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReplaceGrass : MonoBehaviour
+{
+    public GameObject flowerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void ReplaceAll()
+    {
+        ReplaceGrass[] tests = FindObjectsOfType(typeof(ReplaceGrass)) as ReplaceGrass[];
+        foreach (var grass in tests)
+        {
+            Instantiate(flowerPrefab, grass.transform.position, grass.transform.rotation);
+            Destroy(grass.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Seed")
+        {
+            Debug.Log("Seed has touched the grass");
+            ReplaceAll();
+            Destroy(other.gameObject);
+        }
+    }
+}
