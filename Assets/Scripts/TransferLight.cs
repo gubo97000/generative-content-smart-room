786561938f97a0fbaee09609eae36a0e60e0691e
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2021-12-05 18:51:33 +0100

    Almost implement fireflies transfer to mushrooms
    Still has one bug, in which all fireflies detach at once

--- Assets/Scripts/TransferLight.cs
+++ Assets/Scripts/TransferLight.cs
@@ -4,24 +4,50 @@ using UnityEngine;
 
 public class TransferLight : MonoBehaviour
 {
+    public bool isEmpty = true;
+    public GameObject _slot = null;
+
     void Start()
     {
         EventManager.StartListening("ActivateMushroom", ActivateMushroomHandler);
+        EventManager.StartListening("InventoryAddEvent", OnInventoryAddEvent);
     }
 
     void OnDestroy()
     {
         EventManager.StopListening("ActivateMushroom", ActivateMushroomHandler);
+        EventManager.StopListening("InventoryAddEvent", OnInventoryAddEvent);
     }
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Jump" && other.gameObject.GetComponent<JumpCollider>().isInAir())
-            EventManager.TriggerEvent("ActivateMushroom", gameObject);
+            EventManager.TriggerEvent("ActivateMushroom", gameObject, new EventDict() { 
+                { "sender", other.gameObject.transform.parent.gameObject /* i.e. the player object */ }
+            });
     }
 
     void ActivateMushroomHandler(EventDict dict)
     {
+        GameObject sender = (GameObject)dict["sender"];
 
+        if (isEmpty && InventoryManager.HasItemsByTagName(sender, "Firefly"))
+        {
+            EventManager.TriggerEvent("ItemReceived", gameObject, new EventDict() { { "receiver", gameObject }, { "giver", sender }, { "item", InventoryManager.GetItemByTagName(sender, "Firefly") } });
+            isEmpty = false;
+            EventManager.TriggerEvent("MushroomHasFirefly", gameObject);
+        }
+    }
+
+    void OnInventoryAddEvent(EventDict dict)
+    {
+        GameObject owner = (GameObject)dict["owner"];
+        GameObject item = (GameObject)dict["item"];
+        if (owner == gameObject)
+        {
+            _slot = item;
+            EventManager.TriggerEvent("FollowMe", gameObject, new EventDict() { { "receiver", item } });
+            Debug.Log("FollowMe");
+        }
     }
 }
