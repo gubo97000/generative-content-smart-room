acd4efbaf627818e6cba17e615564293cdd29d8a
Gubo97000 <gubo97000@hotmail.it>
2021-12-05 13:54:52 +0100

    bug correction glow disappear when OnHelperGlowEnd

--- Assets/Scripts/GuidoLab/HelperGlow.cs
+++ Assets/Scripts/GuidoLab/HelperGlow.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [RequireComponent(typeof(Outline))]
 public class HelperGlow : MonoBehaviour
 {
+    int callCounter = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -32,10 +33,15 @@ public class HelperGlow : MonoBehaviour
     void OnHandRaiseStart(EventDict dict)
     {
         GetComponent<Outline>().enabled = true;
+        callCounter+=1;
     }
     void OnHandRaiseEnd(EventDict dict)
     {
-        GetComponent<Outline>().enabled = false;
+        callCounter-=1;
+        if (callCounter == 0)
+        {
+            GetComponent<Outline>().enabled = false;
+        }
     }
 
 }
