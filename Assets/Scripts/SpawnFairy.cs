5e3005740a90f62d43911d0a65531b2ac645bc1b
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2021-12-06 16:02:20 +0100

    Implement tree switch on click on fairy

--- Assets/Scripts/SpawnFairy.cs
+++ Assets/Scripts/SpawnFairy.cs
@@ -20,7 +20,7 @@ public class SpawnFairy : MonoBehaviour
 
     void checkAllLilypads(EventDict dict)
     {
-        if (System.Array.TrueForAll(mushrooms, m => !m.GetComponent<TransferLight>().isEmpty))
+        //if (System.Array.TrueForAll(mushrooms, m => !m.GetComponent<TransferLight>().isEmpty))
         {
             spawnFairy();
             EventManager.TriggerEvent("MushroomCleanUp");
