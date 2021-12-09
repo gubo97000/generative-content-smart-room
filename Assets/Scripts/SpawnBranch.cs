ac5d5f9ed21279df8449f911b85f45608f6d1a37
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2021-12-09 20:18:49 +0100

    Spawn seeds from clicked bush

--- Assets/Scripts/SpawnBranch.cs
+++ Assets/Scripts/SpawnBranch.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class SpawnBranch : MonoBehaviour
 {
     public GameObject prefab;
+
+    public float xOffsetMin = -2f, xOffsetMax = 2f;
+    public float yOffsetMin = 3f, yOffsetMax = 3f;
+    public float zOffsetMin = 1f, zOffsetMax = 2f;
+
     private GameObject instance = null;
 
     void OnMouseDown()
     {
         Vector3 position = transform.position;
-        position.x += Random.Range(-10, 10) / 5;
-        position.y += 3;
-        position.z += Random.Range(5, 10) / 5;
+        position.x += Random.Range(5 * xOffsetMin, 5 * xOffsetMax) / 5;
+        position.y += Random.Range(5 * yOffsetMin, 5 * yOffsetMax) / 5;
+        position.z += Random.Range(5 * zOffsetMin, 5 * zOffsetMax) / 5;
 
         if (instance == null)
             instance = Instantiate(prefab, position, Quaternion.Euler(new Vector3(Random.Range(0, 360), Random.Range(0, 360), Random.Range(0, 360))));
-        Debug.Log("A branch fell from the oak tree");
     }
 }
