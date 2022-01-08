03e113612b39cab96cc9342e961fa045d5920a62
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-01-08 22:23:35 +0100

    Fix delay of beavers animator transition
    Do not wait for previous (eating) animation to finish

--- Assets/MoveToDam.cs
+++ Assets/MoveToDam.cs
@@ -7,6 +7,7 @@ public class MoveToDam : MonoBehaviour
     public int howManyBeavers = 3;
     public GameObject[] beavers;
     public GameObject[] newPaths;
+    public float cooldown = 0.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +27,7 @@ public class MoveToDam : MonoBehaviour
     {
         if (System.Array.TrueForAll(beavers, m => m.GetComponent<BeaverAnimationManager>().isBeaverEating()))
         {
-            // Change paths to follow
-            foreach(GameObject b in beavers){
-                b.GetComponent<FollowThePath>().ResetPath(newPaths);
-            }
+            StartCoroutine(changePath());
         }
     }
 
@@ -38,6 +36,16 @@ public class MoveToDam : MonoBehaviour
         GameObject sender = (GameObject)dict["sender"];
         int order = (int)dict["order"];
         beavers[order] = sender;
+    }
+
+    IEnumerator changePath()
+    {
+        // Change paths to follow
+        foreach (GameObject b in beavers)
+        {
+            yield return new WaitForSeconds(cooldown);
+            b.GetComponent<FollowThePath>().ResetPath(newPaths);
+        }
     }
 
 }
