46b84169ea2329abe2e6b28e27588522d2f102d6
Alessandro Atanassov <alessandro.atanassov@mail.polimi.it>
2022-02-02 21:12:27 +0100

    Add magic particles to switch tree

--- Assets/Scripts/GuidoLab/StateManagers/TreesSwitcher.cs
+++ Assets/Scripts/GuidoLab/StateManagers/TreesSwitcher.cs
@@ -6,14 +6,16 @@ using UnityEngine;
 public class TreesSwitcher : ObjectStateHandler
 {
     private int _index = 0;
+    public GameObject spawnParticles;
+    
     //Set the states here, with the scripts attached for each state.
     private void Reset()
     {
         states = new State[]
         {
-    new State("Oak"),
-    new State("Apple"),
-    new State("Honey")
+            new State("Oak"),
+            new State("Apple"),
+            new State("Honey")
         };
     }
 
@@ -32,9 +34,23 @@ public class TreesSwitcher : ObjectStateHandler
     //to change the state change the value of CurrentState
     void OnSwitchTreeForward(EventDict dict)
     {
+        StartCoroutine(Switch());
+    }
+
+    IEnumerator Switch()
+    {
+        Vector3 position = transform.position;
+        //position.y = 1;
+
+        GameObject sp = Instantiate(spawnParticles, /*position +*/ new Vector3(1.71f, 7.07f, -2.27f), Quaternion.identity);
+        sp.GetComponent<ParticleSystem>().Play();
+        yield return new WaitForSeconds(2.5f);
         _index++;
         _index %= states.Length;
         CurrentState = states[_index].name;
-    }
 
+        sp.GetComponent<ParticleSystem>().Stop();
+        yield return new WaitForSeconds(5f);
+        Destroy(sp);
+    }
 }
